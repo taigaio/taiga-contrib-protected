f6eb34ffd966d910905cd74ac35e33a5c65c4035
Miguel Gonzalez <migonzalvar@gmail.com>
2018-07-10 22:54:41 +0200

    feat: Add first implementation

--- taiga_contrib_protected/storage.py
+++ taiga_contrib_protected/storage.py
@@ -1,2 +1,16 @@
-class ProtectedFileSystemStorage:
-    pass
+import urllib.parse
+
+from django.conf import settings
+from django.core.signing import TimestampSigner
+
+from taiga.base.storage import FileSystemStorage
+
+
+class ProtectedFileSystemStorage(FileSystemStorage):
+    def url(self, name):
+        orig = super().url(name)
+        signer = TimestampSigner(settings.SECRET_KEY, sep=':', salt='taiga-protected')
+        signature = signer.sign(name)
+        _, _, token = signature.partition(':')
+        qs = urllib.parse.urlencode({'token': token})
+        return '%s?%s' % (orig, qs)
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,6 +1,31 @@
+from unittest import mock
+
+
+class FileSystemStorageStub:
+    def url(self, path):
+        return 'http://protected.server.test/' + str(path)
+
+
+class TimestampSignerStub:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def sign(self, name):
+        return 'x' * 40
+
+
+# Mock imported modules
+import sys
+sys.modules['django.conf'] = mock.Mock(settings=mock.Mock())
+sys.modules['django.core.signing'] = mock.Mock(TimestampSigner=TimestampSignerStub)
+sys.modules['taiga.base.storage'] = mock.Mock(FileSystemStorage=FileSystemStorageStub)
+
 import taiga_contrib_protected.storage
 
 
 def test_backend_exists():
     backend_class = taiga_contrib_protected.storage.ProtectedFileSystemStorage
     backend = backend_class()
+    url = backend.url('path/to/file.ext')
+    assert url.startswith('http')
+    assert '?token=' in url
